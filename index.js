670f71045d4e1c50d5673f0425090fa77d611883
Ben Hovinga <agnivohneb@gmail.com>
2021-03-03 11:59:01 -0400

    make sounds go brrrrrr

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+const soundsFolder = "./sounds/";
+const soundsExt = ".mp3";
+const drumSounds = {
+  w: "tom-1",
+  a: "tom-2",
+  s: "tom-3",
+  d: "tom-4",
+  j: "snare",
+  k: "crash",
+  l: "kick-bass",
+};
+
 for (const drum of document.querySelectorAll(".drum")) {
-  drum.addEventListener("click", function () {
-    console.log(drum.className);
-  });
+  if (drumSounds.hasOwnProperty(drum.innerHTML)) {
+    drum.addEventListener("click", function () {
+      let audio = new Audio(
+        soundsFolder + drumSounds[this.innerHTML] + soundsExt
+      );
+      audio.play();
+    });
+  }
 }
+
+document.addEventListener("keydown", function (event) {
+  if (drumSounds.hasOwnProperty(event.key)) {
+    let audio = new Audio(soundsFolder + drumSounds[event.key] + soundsExt);
+    audio.play();
+  }
+});
